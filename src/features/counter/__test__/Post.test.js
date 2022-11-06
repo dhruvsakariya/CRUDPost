6282968f1dcb6c72505f928e8ba98895317e972f
Dhruv <dhruvsakariya2304@gmail.com>
2022-11-06 21:29:11 +0530

    jest clash with axios resolved

---
+++ src/features/counter/__test__/Post.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+
+import { store } from "../../../app/store";
+
+test("get post", () => {});
+test("delete post", () => {});
+test("update post", () => {});
+test("create posts", () => {});
